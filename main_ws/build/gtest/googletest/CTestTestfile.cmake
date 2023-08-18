5a90acbcee1a21099debf7b976bda6dcac7cfcbb
Marco Michelini <marco.michelini@protonmail.com>
2023-08-18 11:21:24 +0200

    Added main catkin workspace

---
+++ main_ws/build/gtest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest/googletest
+# Build directory: /home/marco/ROS/smart-robot-chess-companion/main_ws/build/gtest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
