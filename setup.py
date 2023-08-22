f9bf3fc9087bf149452fb4237ec370658bc9e1d3
Marco Michelini <marco.michelini@protonmail.com>
2023-08-22 18:58:19 +0200

    Updated setup script with correct models path

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from distutils.dir_util import copy_tree
 import os
 
-def add_models_to_gazebo_library():
+def add_models_to_gazebo_library(): 
+    src = 'main_ws/src/smart-robot-chess-companion/models'
     dst = f"{os.environ['HOME']}/.gazebo/models"
 
-    for dir in os.listdir('main_ws/src/models/'):
-        copy_tree(f'main_ws/src/smart-robot-chess-companion/models/{dir}', f'{dst}/{dir}')
+    for dir in os.listdir(src):
+        copy_tree(f'{src}/{dir}', f'{dst}/{dir}')
         print(f'Copied model {dir} in {dst}.')
 
 if __name__ == '__main__':
