d127a089547cf9db080b8315594e92cab0878479
Marco Michelini <marco.michelini@protonmail.com>
2023-08-24 13:00:09 +0200

    Added function appending source line to .bashrc file to setup script (executing command source devel/setup.bash for each new terminal isn't necessary anymore)

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from distutils.dir_util import copy_tree
 import os
+import subprocess
 
 def add_models_to_gazebo_library(): 
     src = 'main_ws/src/smart-robot-chess-companion/models'
@@ -9,5 +10,13 @@ def add_models_to_gazebo_library():
         copy_tree(f'{src}/{dir}', f'{dst}/{dir}')
         print(f'Copied model {dir} in {dst}.')
 
+def add_source_line_to_bashrc():
+    bashrc_line = f'source {os.getcwd()}/main_ws/devel/setup.bash'
+    bashrc_path = f"{os.environ['HOME']}/.bashrc"
+    cmd = f'echo "{bashrc_line}" >> {bashrc_path}'
+    subprocess.run(cmd, shell=True)
+    print('Added source line to .bashrc file.')
+
 if __name__ == '__main__':
     add_models_to_gazebo_library()
+    add_source_line_to_bashrc()
