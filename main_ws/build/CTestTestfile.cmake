1383ce6b6a91d65a86be145fa8f60a5f1731eef4
Marco Michelini <56358546+mrcmich@users.noreply.github.com>
2023-08-24 14:37:11 +0200

    Deleted build directory in main catkin workspace

--- main_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/marco/ROS/smart-robot-chess-companion/main_ws/src
-# Build directory: /home/marco/ROS/smart-robot-chess-companion/main_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("smart-robot-chess-companion")
--- main_ws/build/smart-robot-chess-companion/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/marco/ROS/smart-robot-chess-companion/main_ws/src/smart-robot-chess-companion
-# Build directory: /home/marco/ROS/smart-robot-chess-companion/main_ws/build/smart-robot-chess-companion
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
