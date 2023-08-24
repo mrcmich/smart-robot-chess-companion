39e089d7539adb0e2e1569ecc1c4a83bedbb3511
Marco Michelini <marco.michelini@protonmail.com>
2023-08-24 15:06:27 +0200

    Updated setup script to automatically set file permissions and run catkin_make command; executed setup script

--- setup.py
+++ setup.py
@@ -2,6 +2,16 @@ from distutils.dir_util import copy_tree
 import os
 import subprocess
 
+def set_rwx_file_permissions():
+    cmd = 'chmod -R 777 .'
+    subprocess.run(cmd, shell=True)
+    print('Set all files permissions.')
+
+def catkin_make():
+    cmd = 'cd main_ws; catkin_make; cd ..'
+    subprocess.run(cmd, shell=True)
+    print('Executed command catkin_make.')
+
 def add_models_to_gazebo_library(): 
     src = 'main_ws/src/smart-robot-chess-companion/models'
     dst = f"{os.environ['HOME']}/.gazebo/models"
@@ -18,5 +28,7 @@ def add_source_line_to_bashrc():
     print('Added source line to .bashrc file.')
 
 if __name__ == '__main__':
+    set_rwx_file_permissions()
+    catkin_make()
     add_models_to_gazebo_library()
     add_source_line_to_bashrc()
