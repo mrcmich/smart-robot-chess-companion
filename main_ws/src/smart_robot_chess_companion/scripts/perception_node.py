7e4796d411f0f71e72e1cfb49b69e934f062ae7b
Francesco Baraldi <francesco_baraldi@icloud.com>
2023-09-08 13:31:59 +0200

    changed perception node

--- main_ws/src/smart_robot_chess_companion/scripts/perception_node.py
+++ main_ws/src/smart_robot_chess_companion/scripts/perception_node.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
 from typing import Any
+import cv2
 import rospy
 from std_msgs.msg import Bool, String
 from sensor_msgs.msg import Image
@@ -8,6 +9,7 @@ from cv_bridge import CvBridge
 
 import smart_robot_chess_companion.utils as utils
 import smart_robot_chess_companion.perception.config as config
+import smart_robot_chess_companion.perception.predict as predict
 
 
 def update_camera_view_state(message: Any, *args: Any) -> None:
@@ -19,7 +21,7 @@ def update_camera_view_state(message: Any, *args: Any) -> None:
 
 def read_image(message: Any, *args: Any) -> None:
     input = args[0][0]
-    input['image'] = message.data  # TODO: read correctly the image as np.array
+    input['image'] = message  # TODO: read correctly the image as np.array
 
 
 def perception_node() -> None:
@@ -47,13 +49,14 @@ def perception_node() -> None:
     
     board_state_publisher = rospy.Publisher('board_state', String, queue_size=10)
     board_state_publisher.publish(True)
-    
+    ratio = 640 / 720
     while not rospy.is_shutdown():
-        if input['is_camera_view_free'] and input['image'] is not None:
+        #if input['is_camera_view_free'] and input['image'] is not None:
+        if input['image'] is not None:
             print(input['is_camera_view_free'])
-            img = bridge.imgmsg_to_cv2(input['image'], desired_encoding='passthrough')
-            print(type(img))
-            # TODO: read the image, crop and resize it, and make detection
+            img = bridge.imgmsg_to_cv2(input['image'], 'bgr8')
+            resized_img = cv2.resize(img[:, 280:-280, :], (0, 0), fx=ratio, fy=ratio, interpolation=cv2.INTER_AREA)
+            board_state_dict, board_state_matrix = predict.predict(model_checkpoint_path=config.MODEL_CHECKPOINT_PATH, img=resized_img, device='cpu')
             # TODO: create board state and check if it has changed
             # TODO: send the board state to paglia if it has changed
             pass
