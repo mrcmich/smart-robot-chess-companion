6dbfd73a1a46cc64db39bc5d29113613317a1bfa
Francesco Baraldi <francesco_baraldi@icloud.com>
2023-09-08 11:49:38 +0200

    wip: started to code the perception ros node

---
+++ main_ws/src/smart_robot_chess_companion/scripts/perception_node.py
@@ -0,0 +1,65 @@
+#! /usr/bin/env python
+
+from typing import Any
+import rospy
+from std_msgs.msg import Bool, String, Image
+from cv_bridge import CvBridge
+
+from smart_robot_chess_companion import utils
+import smart_robot_chess_companion.perception.config as config
+
+
+def update_camera_view_state(message: Any, *args: Any) -> None:
+    input = args[0][0]
+    input['is_camera_view_free'] = message.data
+    if not message.data:
+        input['image'] = None
+
+
+def read_image(message: Any, *args: Any) -> None:
+    input = args[0][0]
+    input['image'] = message.data  # TODO: read correctly the image as np.array
+
+
+def perception_node() -> None:
+    
+    rospy.init_node('perception_script', anonymous=True)
+    rate = rospy.Rate(1)
+    
+    bridge = CvBridge()
+    
+    input = {'is_camera_view_free': False, 'image': None}
+    
+    camera_view_state_subscriber = rospy.Subscriber(
+        'is_camera_view_free', 
+        Bool, 
+        update_camera_view_state,
+        callback_args=[input]
+    )
+    
+    image_raw_subscriber = rospy.Subscriber(
+        'camera/image_raw', 
+        Image, 
+        read_image,
+        callback_args=[input]
+    )
+    
+    board_state_publisher = rospy.Publisher('board_state', String, queue_size=10)
+    board_state_publisher.publish(True)
+    
+    while not rospy.is_shutdown():
+        if input['is_camera_view_free'] and input['image'] is not None:
+            img = bridge.imgmsg_to_cv2(input['image'], desired_encoding='passthrough')
+            # TODO: read the image, crop and resize it, and make detection
+            # TODO: create board state and check if it has changed
+            # TODO: send the board state to paglia if it has changed
+            pass
+        
+        rate.sleep()
+
+
+if __name__ == '__main__':
+    try:
+        perception_node()
+    except rospy.ROSInterruptException:
+        pass
--- main_ws/src/smart_robot_chess_companion/src/smart_robot_chess_companion/perception/predict.py
+++ main_ws/src/smart_robot_chess_companion/src/smart_robot_chess_companion/perception/predict.py
@@ -1,26 +1,14 @@
-from pathlib import Path
-from argparse import Namespace
-import json
+from typing import Tuple, Dict
 from collections import defaultdict
-import cv2
 import numpy as np
 from ultralytics import YOLO
 
-from utils import parse_arguments
 
-
-def predict(args: Namespace) -> None:
-
-    # model = YOLO(args.checkpoint_path)
-    model = YOLO("/Users/francescobaraldi/Downloads/best.pt")
-    input_dir = Path("/Users/francescobaraldi/Desktop/untitled folder")
-    output_dir = Path("/Users/francescobaraldi/Desktop/chess_dataset/test/pred_images")
-    output_dir.mkdir(exist_ok=True, parents=True)
+def predict(model_checkpoint_path: str, img: np.ndarray, device: str) -> Tuple[Dict[str, Dict[str, str]], np.ndarray]:
     
     cell_width = 63  # px
     origin = 70  # px
     ranges = np.array([[origin + cell_width * i, origin + cell_width * (i + 1) + 1] for i in range (8)])
-    
     piece_2_name = {
         'rook': 'r',
         'knight': 'n',
@@ -29,42 +17,32 @@ def predict(args: Namespace) -> None:
         'king': 'k',
         'pawn': 'p'
     }
+        
+    chess_state_dict = {}
+    piece_counter = defaultdict(int)
+    chess_state_matrix = np.full((8, 8), fill_value=-1, dtype=np.int8)
     
-    for image_filename in input_dir.glob("*"):
-        if image_filename.stem.startswith("."):
-            continue
-        chess_state = {}
-        piece_counter = defaultdict(int)
-        chess_state_matrix = np.full((8, 8), fill_value=-1, dtype=np.int8)
-        results = model.predict(str(image_filename), imgsz=args.image_size_h, conf=0.5, device=args.device, save_txt=False, save=False)
-        for cur_result in results:
-            img = cur_result.orig_img
-            class_names = cur_result.names
-            image_h, image_w = cur_result.orig_shape
-            bboxes = cur_result.boxes.xywhn.numpy()
-            classes = cur_result.boxes.cls.numpy()
-            for cls_idx, (x, y, w, h) in zip(classes, bboxes):
-                piece, player = class_names[cls_idx].split("_")
-                piece_counter[class_names[cls_idx]] += 1
-                x, w = int(x * image_w), int(w * image_w)
-                y, h = int(y * image_h), int(h * image_w)
-                color = (0, 0, 0) if player == 'black' else (255, 255, 255)
-                cv2.rectangle(img, (x - w // 2, y - h // 2), ((x + (w - w // 2), y + (h - h // 2))), color=color, thickness=2)
-                cv2.putText(img, piece, org=(x - w // 2, y - h // 2 - 10), fontFace=cv2.FONT_HERSHEY_COMPLEX_SMALL, fontScale=1, color=color, thickness=2)
-                cv2.imwrite(str(output_dir / image_filename.name), img)
-                cell_col = np.where((x >= ranges[:, 0]) & (x < ranges[:, 1]))[0].item(0)
-                cell_row = 7 - np.where((y >= ranges[:, 0]) & (y < ranges[:, 1]))[0].item(0)
-                chess_state_matrix[cell_row, cell_col] = cls_idx
-                key = f'{player}_{piece}'
-                if not piece in ['queen', 'king']:
-                    key += f'_{piece_counter[class_names[cls_idx]]}'
-                chess_state[key] = {'name': piece_2_name[piece], 'col': cell_col, 'row': cell_row, 'player': player}
-            chess_state_json_str = json.dumps(chess_state)
-            # send to paglia the json str if the state has changed
-            
-
-if __name__ == "__main__":
+    image_h, image_w = img.shape
     
-    args = parse_arguments()
-    assert args.image_size_h == args.image_size_w, "ERROR: during training image_size_h must be equal to image_size_w"
-    predict(args=args)
+    model = YOLO(model_checkpoint_path)
+    detections = model.predict(img, imgsz=image_h, conf=0.5, device=device, save_txt=False, save=False)
+    
+    for cur_detection in detections:
+        class_names = cur_detection.names
+        image_h, image_w = cur_detection.orig_shape
+        bboxes = cur_detection.boxes.xywhn.numpy()
+        classes = cur_detection.boxes.cls.numpy()
+        for cls_idx, (x, y, w, h) in zip(classes, bboxes):
+            piece, player = class_names[cls_idx].split("_")
+            piece_counter[class_names[cls_idx]] += 1
+            x, w = int(x * image_w), int(w * image_w)
+            y, h = int(y * image_h), int(h * image_w)
+            cell_col = np.where((x >= ranges[:, 0]) & (x < ranges[:, 1]))[0].item(0)
+            cell_row = 7 - np.where((y >= ranges[:, 0]) & (y < ranges[:, 1]))[0].item(0)
+            chess_state_matrix[cell_row, cell_col] = cls_idx
+            key = f'{player}_{piece}'
+            if not piece in ['queen', 'king']:
+                key += f'_{piece_counter[class_names[cls_idx]]}'
+            chess_state_dict[key] = {'name': piece_2_name[piece], 'col': cell_col, 'row': cell_row, 'player': player}
+        
+    return chess_state_dict, chess_state_matrix
